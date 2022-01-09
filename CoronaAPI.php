ca5e44ee7a5687d8efdce9204613748e87dc65b5
TrFolwe <78105136+TrFolwe@users.noreply.github.com>
2022-01-09 03:34:28 +0300

    update added date

--- CoronaAPI.php
+++ CoronaAPI.php
@@ -1,7 +1,7 @@
 <?php
 
 class CoronaAPI {
-
+	
 	public function __construct() {
 		echo $this->returnToCoronaInfo("tests"); //Number of tests available
 		echo $this->returnToCoronaInfo("cases"); //Number of current cases
@@ -9,9 +9,10 @@ class CoronaAPI {
 		echo $this->returnToCoronaInfo("recovered"); //Number of recoveries available
 	}
 
-	public function getCoronaInfo(string $data = "cases") :string {
+	public function getCoronaInfo(string $data = "cases", ?string $date = null) :string {
+        $date = $date ?? date("d/m/Y");
         $corona_site = json_decode(file_get_contents("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json", true), true);
-        return number_format(intval(array_key_exists(date("d/m/Y"), $corona_site) ? $corona_site[date("d/m/Y")][$data] : $corona_site[date("d/m/Y", strtotime("yesterday"))][$data]), 0, ".",".");
+        return number_format(intval(array_key_exists($date, $corona_site) ? $corona_site[$date][$data] : $corona_site[date("d/m/Y", strtotime("yesterday"))][$data]), 0, ".",".");
     }
 }
 new CoronaAPI();
